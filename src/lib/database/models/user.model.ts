0a751ae1ca0c2fd5d3f316a0bde77bc6f2f8067c
chiweily <mepiod@gmail.com>
2024-08-06 19:22:53 +0800

    update

--- src/lib/database/models/user.model.ts
+++ src/lib/database/models/user.model.ts
@@ -1,10 +1,43 @@
 // clerkId, email, userName, photo, firstName, lastName, planId, creditBalance
 
+import { model, models, Schema } from "mongoose"
+
 // interface
 export interface User {
-    
+    _id: string,
+    clerkId: string,
+    email: string,
+    userName: string,
+    photo: string,
+    firstName: string,
+    lastName: string,
+    planId: string,
+    creditBalance: number,
+    createdAt: Date,
+    updatedAt: Date
 }
 
-// schema
+const UserSchema = new Schema({
+    clerkId: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true
+    },
+    photo: {
+        type: String,
+        required: true
+    },
+    firstName: {
+        type: String
+    }
+})
 
-// model
+const User = models?.User || model('User', UserSchema)
+export default User
